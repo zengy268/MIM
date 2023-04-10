c1c2956e09668997979fcfa12b7cef65fb82a083
zengy268 <110041581+zengy268@users.noreply.github.com>
2023-04-10 22:02:35 +0800

    Add files via upload

---
+++ utils/data_preparation.py
@@ -0,0 +1,245 @@
+import random
+import argparse
+import numpy as np
+from global_configs import config
+from transformers import BertTokenizer, XLNetTokenizer
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import pickle
+
+
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(self, input_ids, visual, acoustic, input_mask, segment_ids, label_id):
+        self.input_ids = input_ids
+        self.visual = visual
+        self.acoustic = acoustic
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_id = label_id
+
+
+def prepare_bert_input(tokens, visual, acoustic, tokenizer):
+    CLS = tokenizer.cls_token
+    SEP = tokenizer.sep_token
+    tokens = [CLS] + tokens + [SEP]
+
+    # Pad zero vectors for acoustic / visual vectors to account for [CLS] / [SEP] tokens
+    acoustic_zero = np.zeros((1, config.ACOUSTIC_DIM))
+    acoustic = np.concatenate((acoustic_zero, acoustic, acoustic_zero))
+    visual_zero = np.zeros((1, config.VISUAL_DIM))
+    visual = np.concatenate((visual_zero, visual, visual_zero))
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+    segment_ids = [0] * len(input_ids)
+    input_mask = [1] * len(input_ids)
+
+    pad_length = config.max_seq_length - len(input_ids)
+
+    acoustic_padding = np.zeros((pad_length, config.ACOUSTIC_DIM))
+    acoustic = np.concatenate((acoustic, acoustic_padding))
+
+    visual_padding = np.zeros((pad_length, config.VISUAL_DIM))
+    visual = np.concatenate((visual, visual_padding))
+
+    padding = [0] * pad_length
+
+    # Pad inputs
+    input_ids += padding
+    input_mask += padding
+    segment_ids += padding
+
+    return input_ids, visual, acoustic, input_mask, segment_ids
+
+
+def prepare_xlnet_input(tokens, visual, acoustic, tokenizer):
+    CLS = tokenizer.cls_token
+    SEP = tokenizer.sep_token
+    PAD_ID = tokenizer.pad_token_id
+
+    # PAD special tokens
+    tokens = tokens + [SEP] + [CLS]
+    audio_zero = np.zeros((1, config.ACOUSTIC_DIM))
+    acoustic = np.concatenate((acoustic, audio_zero, audio_zero))
+    visual_zero = np.zeros((1, config.VISUAL_DIM))
+    visual = np.concatenate((visual, visual_zero, visual_zero))
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+    input_mask = [1] * len(input_ids)
+    segment_ids = [0] * (len(tokens) - 1) + [2]
+
+    pad_length = (config.max_seq_length - len(segment_ids))
+
+    # then zero pad the visual and acoustic
+    audio_padding = np.zeros(pad_length, config.ACOUSTIC_DIM)
+    acoustic = np.concatenate((audio_padding, acoustic))
+
+    video_padding = np.zeros(pad_length, config.VISUAL_DIM)
+    visual = np.concatenate((video_padding, visual))
+
+    input_ids = [PAD_ID] * pad_length + input_ids
+    input_mask = [0] * pad_length + input_mask
+    segment_ids = [3] * pad_length + segment_ids
+
+    return input_ids, visual, acoustic, input_mask, segment_ids
+
+
+def convert_to_features(examples, max_seq_length, tokenizer):
+    features = []
+
+    for (ex_index, example) in enumerate(examples):
+
+        (words, visual, acoustic), label_id, segment = example
+
+        tokens, inversions = [], []
+        for idx, word in enumerate(words):
+            tokenized = tokenizer.tokenize(word)
+            tokens.extend(tokenized)
+            inversions.extend([idx] * len(tokenized))
+
+        # Check inversion
+        assert len(tokens) == len(inversions)
+
+        aligned_visual = []
+        aligned_audio = []
+
+        for inv_idx in inversions:
+            aligned_visual.append(visual[inv_idx, :])
+            aligned_audio.append(acoustic[inv_idx, :])
+
+        visual = np.array(aligned_visual)
+        acoustic = np.array(aligned_audio)
+
+        # Truncate input if necessary
+        if len(tokens) > config.max_seq_length - 2:
+            tokens = tokens[: config.max_seq_length - 2]
+            acoustic = acoustic[: config.max_seq_length - 2]
+            visual = visual[: config.max_seq_length - 2]
+
+        if config.model_choice == "bert-base-uncased":
+            prepare_input = prepare_bert_input
+        elif config.model_choice == "xlnet-base-cased":
+            prepare_input = prepare_xlnet_input
+
+        input_ids, visual, acoustic, input_mask, segment_ids = prepare_input(
+            tokens, visual, acoustic, tokenizer
+        )
+
+        # Check input length
+        assert len(input_ids) == config.max_seq_length
+        assert len(input_mask) == config.max_seq_length
+        assert len(segment_ids) == config.max_seq_length
+        assert acoustic.shape[0] == config.max_seq_length
+        assert visual.shape[0] == config.max_seq_length
+
+        features.append(
+            InputFeatures(
+                input_ids=input_ids,
+                input_mask=input_mask,
+                segment_ids=segment_ids,
+                visual=visual,
+                acoustic=acoustic,
+                label_id=label_id,
+            )
+        )
+    return features
+
+
+def get_tokenizer(model):
+    if model == "bert-base-uncased":
+        return BertTokenizer.from_pretrained(model)
+    elif model == "xlnet-base-cased":
+        return XLNetTokenizer.from_pretrained(model)
+    else:
+        raise ValueError(
+            "Expected 'bert-base-uncased' or 'xlnet-base-cased, but received {}".format(
+                model
+            )
+        )
+
+
+def get_appropriate_dataset(data):
+
+    tokenizer = get_tokenizer(config.model_choice)
+
+    features = convert_to_features(data, config.max_seq_length, tokenizer)
+    all_input_ids = torch.tensor(
+        [f.input_ids for f in features], dtype=torch.long)
+    all_input_mask = torch.tensor(
+        [f.input_mask for f in features], dtype=torch.long)
+    all_segment_ids = torch.tensor(
+        [f.segment_ids for f in features], dtype=torch.long)
+    all_visual = torch.tensor([f.visual for f in features], dtype=torch.float)
+    all_acoustic = torch.tensor(
+        [f.acoustic for f in features], dtype=torch.float)
+    all_label_ids = torch.tensor(
+        [f.label_id for f in features], dtype=torch.float)
+
+    dataset = TensorDataset(
+        all_input_ids,
+        all_visual,
+        all_acoustic,
+        all_input_mask,
+        all_segment_ids,
+        all_label_ids,
+    )
+    return dataset
+
+
+def set_up_data_loader(dataset, train_batch_size, dev_batch_size, test_batch_size, n_epochs):
+    with open(f"datasets/{dataset}.pkl", "rb") as handle:
+        data = pickle.load(handle)
+
+    train_data = data["train"]
+    dev_data = data["dev"]
+    test_data = data["test"]
+
+    train_dataset = get_appropriate_dataset(train_data)
+    dev_dataset = get_appropriate_dataset(dev_data)
+    test_dataset = get_appropriate_dataset(test_data)
+
+    num_train_optimization_steps = (
+        int(
+            len(train_dataset) / train_batch_size
+        )
+        * n_epochs
+    )
+
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=train_batch_size, shuffle=True, drop_last=True
+    )
+
+    dev_dataloader = DataLoader(
+        dev_dataset, batch_size=dev_batch_size, shuffle=True
+    )
+
+    test_dataloader = DataLoader(
+        test_dataset, batch_size=test_batch_size, shuffle=True,
+    )
+
+    return (
+        train_dataloader,
+        dev_dataloader,
+        test_dataloader,
+        num_train_optimization_steps,
+    )
+
+
+
+
+
+def seed(s):
+    if isinstance(s, int):
+        if 0 <= s <= 9999:
+            return s
+        else:
+            raise argparse.ArgumentTypeError(
+                "Seed must be between 0 and 2**32 - 1. Received {0}".format(s)
+            )
+    elif s == "random":
+        return random.randint(0, 9999)
+    else:
+        raise argparse.ArgumentTypeError(
+            "Integer value is expected. Recieved {0}".format(s)
+        )
---
+++ utils/utils.py
@@ -0,0 +1,61 @@
+import os
+import torch
+import random
+import numpy as np
+import sys
+
+def set_random_seed(seed: int):
+    """
+    Helper function to seed experiment for reproducibility.
+    If -1 is provided as seed, experiment uses random seed from 0~9999
+
+    Args:
+        seed (int): integer to be used as seed, use -1 to randomly seed experiment
+    """
+   # seed = 9388
+    seed = 5576
+    # print("Seed: {}".format(seed))
+
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.deterministic = True
+
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+def mkdirs(checkpoint_path, best_model_path, logs):
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+    if not os.path.exists(best_model_path):
+        os.makedirs(best_model_path)
+    if not os.path.exists(logs):
+        os.makedirs(logs)
+
+
+class Logger(object):
+    def __init__(self):
+        self.terminal = sys.stdout
+        self.file = None
+
+    def open(self, file, mode = None):
+        if mode is None:
+            mode = 'w'
+        self.file = open(file, mode)
+
+    def write(self, message, is_terminal = 1, is_file = 1):
+        if '\r' in message:
+            is_file = 0
+        if is_terminal == 1:
+            self.terminal.write(message)
+            self.terminal.flush()
+        if is_file == 1:
+            self.file.write(message)
+            self.file.flush()
+
+    def flush(self):
+        pass
